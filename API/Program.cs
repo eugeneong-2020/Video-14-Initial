77eb6ab38a3916636bdab0485055f0106bb295ef
Eugene Ong <eugeneong2021@gmail.com>
2021-05-16 07:29:41 +0800

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConfigureLogger();
+            Log.Information("Starting Application");
+            try
+            {
+                CreateHostBuilder(args).Build().MigrateDatabase().SeedDatabase().Run();
+            }
+            finally
+            {
+                Log.Information("Closing Application");
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static void ConfigureLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    public static class IHostExtensions
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+            context.Database.Migrate();
+
+            return host;
+        }
+        public static IHost SeedDatabase(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+            SeedData.Initialize(context);
+            return host;
+        }
+    }
+
+}
